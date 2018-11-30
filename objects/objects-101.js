2a0855c77e6a4d26759693a595b38159d58e8b26
ShemTheDev <sgyll@protonmail.com>
2018-11-30 23:30:23 +0200

    Added Object folder and objects-101.js

---
+++ objects/objects-101.js
@@ -0,0 +1,28 @@
+let myBook = {
+    title: 'Erty Adentures',
+    author: 'Hrerj',
+    pageCount: 42069
+}
+
+console.log(`${myBook.title} by ${myBook.author} which has ${myBook.pageCount} pages`)
+
+myBook.title = 'Fattigook'
+
+console.log(`${myBook.title} by ${myBook.author} which has ${myBook.pageCount} pages`)
+
+// Challenge area
+// name, age, location
+// [name] is [age] and lives in [location]
+// Increase age by 1 and print message again
+
+let person = {
+    name: 'Yuki Tuki',
+    age: 4,
+    location: 'Hrooken Realm'
+}
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
+
+person.age = 69
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)

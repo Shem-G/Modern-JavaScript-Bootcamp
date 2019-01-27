47d794d2cbe91107a152728c8d16eabd8a111055
ShemTheDev <sgyll@protonmail.com>
2019-01-27 18:04:37 +0200

    Objects section complete

---
+++ objects/number-methods.js
@@ -0,0 +1,26 @@
+let num = 103.941
+
+console.log(num.toFixed(2))
+
+console.log(Math.round(num))
+console.log(Math.floor(num))
+console.log(Math.ceil(num))
+
+let min = 0
+let max = 1
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+console.log(randomNum)
+
+// Challenge area
+// 1 - 5, if guess is correct true, false if not
+let makeGuess = function (guess){
+    let min = 1
+    let max = 5
+    let result = Math.floor(Math.random() * (max - min + 1)) + min
+    console.log(result)
+   
+    return guess === result
+}
+
+console.log(makeGuess(5))
+

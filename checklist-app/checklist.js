f0cbd38cc1f306e5aa554edf9a8faf894c97f3f9
Shem Gyll <shinthedev@protonmail.com>
2019-02-09 15:30:22 +0000

    completed lecture 57

---
+++ checklist-app/checklist.js
@@ -0,0 +1,13 @@
+const checklists = [{
+    title: '10 pushups',
+    description: 'Do 10 pushups per day',
+    quantity: '10',
+    checks: []
+}]
+
+document.querySelector('#create-checklist').addEventListener('click', function(c){
+    const newCheck =checklists.push()
+        const title = document.querySelector('#title-input').textContent
+        const description = document.querySelector('#description-input').textContent
+        const title = document.querySelector('#title-input').valku
+})
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,21 +15,52 @@ const todos = [{
     completed: false
 }]
 
+//1. setup div for todos
+//2. setup filters (searchText) and wire up a new filter
+//3. Create a renderTodos function to render and rerender the filtered data
 
-document.querySelector('#add-todo').addEventListener('click', function(e){
-    console.log('Todo added')
-})
+//starts
 
-const incompleteTodos = todos.filter(function (todo){
+const filters = {
+    searchText: ''
+}
+
+const renderTodos = function(todos, filters){
+    const filteredTodos = todos.filter(function (todo){
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    const incompleteTodos = filteredTodos.filter(function (todo){
         return !todo.completed
     })
 
-const message = document.createElement('p')
-message.textContent = `You have todos ${incompleteTodos.length} left`
-document.querySelector('body').appendChild(message)
+    document.querySelector('#todo-list').innerHTML = ''
 
-todos.forEach(function(todo){
-    const newTodo = document.createElement('p')
-    newTodo.textContent = todo.text
-    document.querySelector('body').appendChild(newTodo)
+    const summary = document.createElement('p')
+    summary.textContent = `You have todos ${incompleteTodos.length} left`
+    document.querySelector('#todo-list').appendChild(summary)
+
+    filteredTodos.forEach(function (e){
+        const p = document.createElement('p')
+        p.innerText = e.text
+        document.querySelector('#todo-list').appendChild(p)
+    })
+ }
+
+renderTodos(todos, filters)
+
+
+document.querySelector('#add-todo').addEventListener('click', function (e){
+console.log('Add a new todo')
+})
+
+//Listen for todo text change
+document.querySelector('#new-todo-text').addEventListener('input', function(e){
+    console.log(e.target.value)
+})
+
+document.querySelector('#search-todo').addEventListener('input', function(e){
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+    
 })
